40909f6070ae9839d531bd1220b092671d8004f2
tutto2 <tuto.2@hotmail.com>
2023-03-30 15:07:55 -0500

    Final

--- src/file_sorting.rb
+++ src/file_sorting.rb
@@ -1,42 +1,19 @@
 require 'fileutils'
+require 'pry'
 
 class FileSorting
   def self.run!
-    dir_name = "../result"
-    Dir.mkdir(dir_name) unless Dir.exists?(dir_name)
-    Dir.chdir("../result") do
-      ('a'..'z').to_a.each do |l| 
-        FileUtils.mkdir_p("./#{l}") unless Dir.exists?("./#{l}")
-      end
+    Dir.mkdir("./result") unless Dir.exists?("./result")
+    ('a'..'z').to_a.each do |l| 
+      FileUtils.mkdir_p("./result/#{l}") unless Dir.exists?("./result/#{l}")
     end
       
-    Dir.chdir("../test_cases") do 
-      files = Dir.entries(".")
-      text = files.each do |f|
-        File.open("#{f}").readline
-      end 
-      file_name = text.map do |t|
-        t.gsub("title ", "")
-      end
+    files = Dir.glob("./test_cases/*")
+    files.each do |f| 
+      title = File.readlines("#{f}").first.chomp.split.last
+      text = File.read("#{f}")
+      File.write("./result/#{title[0]}/#{title}", text)
     end
-            
-    Dir.chdir(dir_name) do
-    d = Dir.glob("*")
-    # first_letter = file_name.map { |a| a[0] }
-    file_name.each do |name, d|
-      if name[0] == "#{d}"
-        Dir.chdir("./#{d}") do 
-        File.new("#{name}") unless File.exist?("#{name}")
-        end
-      end
-    end
-  end
-
-    # file_name.each do |d, file_name|
-    #   Dir.chdir("./#{d}") do
-    #   if "#{first_letter}" == "#{d}"
-    #     File.new("#{file_name}")
-    #   end
   end
 end
 
